57d489f789a2e428d03cb891de2b512cf2873123
anguianoo <njanguiano@gmail.com>
2025-01-18 15:40:13 -0800

    moved files, modified start script to node backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -1,54 +1,51 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
-const cors = require('cors');
+const express = require("express")
+const bodyParser = require("body-parser")
+const axios = require("axios")
+const cors = require("cors")
 
-require('dotenv').config();
-const apiKey = process.env.SPOONACULAR_API_KEY;
+require("dotenv").config()
+const apiKey = process.env.SPOONACULAR_API_KEY
 
-const app = express();
-const port = process.env.PORT;
-
+const app = express()
+const port = process.env.PORT
 
 // setup middleware
-app.use(bodyParser.json());
-app.use(cors()); // enable CORS - needed during development
+app.use(bodyParser.json())
+app.use(cors()) // enable CORS - needed during development
 
 // TODO: setup mongoose connection
 // TODO: connect to MongoDB
 
 // setup connection to Spoonacular API
-const baseUrl = 'https://api.spoonacular.com/recipes/findByIngredients';
-const url = `${baseUrl}?apiKey=${apiKey}`;
+const baseUrl = "https://api.spoonacular.com/recipes/findByIngredients"
+const url = `${baseUrl}?apiKey=${apiKey}`
 
 // setup routes
 // base route
-app.get('/', (res) => {
-  res.send('Hello from the WasteNot API!');
-});
+app.get("/", (req, res) => {
+  res.send("Hello from the WasteNot API!")
+})
 
 // route to handle recipe search
-app.post('/recipes', async (req, res) => {
-  const { ingredients } = req.body;
+app.post("/recipes", async (req, res) => {
+  const { ingredients } = req.body
 
   if (!ingredients) {
-    return res.status(400).json({ error: 'Ingredients parameter is required.' });
+    return res.status(400).json({ error: "Ingredients parameter is required." })
   }
 
-  const urlWithIngredients = `${url}&ingredients=${ingredients}&number=3`; // limit to 3 recipes for now
-  
+  const urlWithIngredients = `${url}&ingredients=${ingredients}&number=3` // limit to 3 recipes for now
+
   try {
-    const response = await axios.get(urlWithIngredients);
-    res.status(200).json(response.data);
-
+    const response = await axios.get(urlWithIngredients)
+    res.status(200).json(response.data)
   } catch (error) {
-    console.error('(Server) Error fetching recipes from Spoonacular:', error);
-    res.status(500).json({ error: 'Error fetching recipes from API' });
+    console.error("(Server) Error fetching recipes from Spoonacular:", error)
+    res.status(500).json({ error: "Error fetching recipes from API" })
   }
-});
+})
 
 // start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
-
+  console.log(`Server running on http://localhost:${port}`)
+})
