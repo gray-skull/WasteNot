1e0c0e77c49d34c15ab34c6f6c12f19769693305
gray-skull <cruisemoose86@gmail.com>
2025-01-19 20:51:59 -0500

    - update CSS styling - update HTML generated by script.js

--- script.js
+++ script.js
@@ -22,12 +22,19 @@ document.addEventListener('DOMContentLoaded', async function () {
       }
 
       const data = await response.json();
+      
+      const searchResults = document.getElementById('search-results');
+      searchResults.innerHTML = `<h2>Recipes using: ${ingredients}</h2>
+                                 <ul id="recipes-list"></ul>
+                                 `;
+      
       const recipesList = document.getElementById('recipes-list');
       recipesList.innerHTML = '';
 
       if (data.length === 0) {
         recipesList.innerHTML = '<li>No recipes found</li>';
       } else {
+
         data.forEach((recipe) => {
           const li = document.createElement('li');
           // TODO: make the <li> a link to the recipe page and include more details
@@ -35,9 +42,14 @@ document.addEventListener('DOMContentLoaded', async function () {
                           <section class="image-box">
                           <img src="${recipe.image}" alt="${recipe.title}" />
                           </section>
+                          <h3>Used ingredients:</h3>
+                          <p>${recipe.usedIngredients.map(ingredient => ingredient.name).join(', ')}</p>
+                          <h3>Additional ingredients:</h3>
+                          <p>${recipe.missedIngredients.map(ingredient => ingredient.name).join(', ')}</p>
                           `;
           recipesList.appendChild(li);
         });
+
       }
     } catch (error) {
       console.error(error);
